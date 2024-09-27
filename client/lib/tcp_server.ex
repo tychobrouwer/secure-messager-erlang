45b744eb84b711b534ad8ad6a9bb99a3b7938f45
Tycho Brouwer <tycho.tbrouwer@gmail.com>
2024-09-27 14:04:23 +0200

    fix receiving and sending messages + switch from packet line to packet with length of two bytes + uuid communication

--- client/lib/tcp_server.ex
+++ client/lib/tcp_server.ex
@@ -4,10 +4,16 @@ defmodule TCPServer do
   """
 
   require Logger
+  @type packet_version :: 1
+  @type packet_type :: :message | :handshake | :ack | :error
+
+  @doc """
+  Accept incoming connections on a given port.
+  """
 
   @spec connect(charlist, integer) :: :ok
   def connect(address, port) do
-    case :gen_tcp.connect(address, port, [:binary, packet: :line, active: true, reuseaddr: true]) do
+    case :gen_tcp.connect(address, port, [:binary, packet: 2, active: false, reuseaddr: true]) do
       {:ok, socket} ->
         Logger.info("Connection on #{address}:#{port}")
 
@@ -26,23 +32,25 @@ defmodule TCPServer do
     end
   end
 
-  defp loop_serve(socket) do
-    receive do
-      # Handling incoming packets (in active mode)
-      {:tcp, ^socket, data} ->
-        Logger.info("Received: #{data}")
+  @doc """
+  Loop to accept incoming connections.
+  """
 
-      # Handling TCP closed connection
+  defp loop_serve(socket) do
+    :inet.setopts(socket, [{:active, :once}])
+
+    receive do
+      {:tcp, ^socket, data} ->
+        handle_data(data, socket)
+
       {:tcp_closed, ^socket} ->
         Logger.error("Connection closed")
         exit(:disconnect)
 
-      # Handling TCP errors
       {:tcp_error, ^socket, reason} ->
         Logger.error("TCP Error: #{reason}")
         exit(:error)
 
-      # Handle sending messages to the socket
       {:send_message, message} ->
         send_data(message, socket)
     after
@@ -50,22 +58,79 @@ defmodule TCPServer do
         nil
     end
 
-    # Loop back to serve
     loop_serve(socket)
   end
 
-  # Function to send a message to the server
+  @doc """
+  Handle incoming data.
+  """
+
+  @spec handle_data(binary, pid) :: :ok
+  defp handle_data(data, socket) do
+    <<packet_version::binary-size(1), packet_type::binary-size(1), message::binary>> = data
+
+    type =
+      case packet_type do
+        <<0>> -> :message
+        <<1>> -> :handshake
+        <<2>> -> :ack
+        <<3>> -> :error
+      end
+
+    case type do
+      :message ->
+        Logger.info("Received message: #{message}")
+
+      :handshake ->
+        Logger.info("Received handshake: #{message}")
+
+      :ack ->
+        Logger.info("Received ack: #{message}")
+
+      :error ->
+        Logger.error("Received error: #{message}")
+    end
+  end
+
+  @doc """
+  Send data to the server.
+  """
+
+  @spec send_data(binary, pid) :: :ok
   defp send_data(message, socket) do
-    case :gen_tcp.send(socket, message) do
+    packet = create_packet(1, type, message)
+
+    case :gen_tcp.send(socket, packet) do
       :ok ->
-        Logger.info("Sent: #{message}")
+        Logger.info("Sent: #{Base.encode16(packet, case: :lower)}")
 
       {:error, reason} ->
         Logger.error("Failed to send message: #{reason}")
     end
   end
 
-  # Public API to send a message from another process
+  @doc """
+  Create a packet to send to the server with a version, type, and data.
+  """
+
+  @spec create_packet(packet_version, packet_type, binary) :: binary
+  def create_packet(version, packet_type, data) do
+    type =
+      case packet_type do
+        :message -> 0
+        :handshake -> 1
+        :ack -> 2
+        :error -> 3
+      end
+
+    <<version::8, type::8, data::binary>>
+  end
+
+  @doc """
+  Public API for sending a message to the server.
+  """
+
+  @spec send_message(integer, packet_type, binary) :: :ok | {:error, any}
   def send_message(pid, message) do
     send(pid, {:send_message, message})
   end
--- server/test/server_test.exs
+++ server/test/server_test.exs
@@ -1,8 +1,14 @@
-defmodule ServerTest do
+defmodule TCPServerTest do
   use ExUnit.Case
-  doctest Server
+  doctest TCPServer
 
-  test "greets the world" do
-    assert Server.hello() == :world
+  require Logger
+
+  test "create_packet" do
+    packet_type = :message
+    packet_version = 1
+    message = "Hello, world!"
+
+    packet = TCPServer.create_packet(packet_version, packet_type, message)
   end
 end
